08a42b90b53fb5f2afc74b5028aef6f8b811448c
KlimekM <mklimek15@gmail.com>
2016-05-06 12:06:49 -0500

    Fix typo in picture div

--- app/javascripts/services.js
+++ app/javascripts/services.js
@@ -7,7 +7,7 @@ angular.module("realEstProperties.services", [])
   o.fetchProperties = function() {
     return $http({
       method: "GET",
-      url: "https://buildout.com/api/v1/KEY/properties.json?limit=20"
+      url: "https://buildout.com/api/v1/543996f9d8bbdbe292c5bf74a414b277ace44313/properties.json?limit=20"
     }).success(function(responseData) {
       o.listings = responseData.properties;
     });
