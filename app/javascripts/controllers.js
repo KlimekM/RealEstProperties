f70dc86f0234a768bc55f262035602274b5253e9
KlimekM <mklimek15@gmail.com>
2016-05-09 11:33:00 -0500

    Format small pictures under map/large photo

--- app/javascripts/controllers.js
+++ app/javascripts/controllers.js
@@ -1,7 +1,7 @@
 angular.module("realEstProperties.controllers", ["uiGmapgoogle-maps"])
 .config(function(uiGmapGoogleMapApiProvider) {
     uiGmapGoogleMapApiProvider.configure({
-        key: "KEY",
+        key: "AIzaSyDeoXJYortVfEO5sGyfRpO5odY0E6Jzhxs",
         v: '3.20',
         libraries: 'weather,geometry,visualization'
     });
